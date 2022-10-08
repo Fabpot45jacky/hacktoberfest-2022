ab441ec87b80d186a3edf214e5a629591cc65fb0
Fabpot45jacky <hanoilion123@gmail.com>
2022-10-08 15:41:39 +0530

    vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // this is declartation of vector
+    vector<int>v;
+    // inserting element in vector
+    v.push_back(90);
+    // output first element of vector
+    cout<<v[0];
+
+}
